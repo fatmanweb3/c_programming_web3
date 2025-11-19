a94905a893ce28845770f75ed32096f58961886d
fatmanweb3 <appubaktha@gmail.com>
2025-11-19 09:22:41 +0530

    str length cal

---
+++ projects/string_length_finder/slf.c
@@ -0,0 +1,15 @@
+#incldue <stdio.h>
+#include <stdlib.h>
+
+size_t str_length(char *str)
+{
+
+}
+
+int main()
+{
+	char input[100];
+	printf("type the string to find the length: ");
+	scanf("%99[^\n]", input);
+
+	printf(" length = %zu\n",
